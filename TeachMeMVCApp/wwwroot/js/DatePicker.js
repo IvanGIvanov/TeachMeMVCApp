d4c8bec8cc778be7a653ab02202f59adabe52b47
Ivan <ivanov.g.ivan@outlook.com>
2022-01-31 01:56:44 +0200

    Implement jQuery for DatePicker in CategoryItem "Create" and "Edit" views.

---
+++ TeachMeMVCApp/wwwroot/js/DatePicker.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+
+    function WireUpDatePicker() {
+
+        const addMonths = 2;
+        const currDate = new Date();
+
+        $('.datepicker').datepicker(
+            {
+                dateFormat: 'yy-mm-dd',
+                minDate: currDate,
+                maxDate: AddSubstractMonths(currDate, addMonths)
+            }
+        );
+    }
+
+    WireUpDatePicker();
+
+});
