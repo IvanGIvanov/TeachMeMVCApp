3187ebcf53a9d114c72258dace09a331bdb11254
Ivan <ivanov.g.ivan@outlook.com>
2022-02-07 23:42:20 +0200

    Add Login/Logout Controller

---
+++ TeachMeMVCApp/Controllers/UserAuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeachMeMVCApp.Models;
+using TeeachMeMVCWebApp.Data;
+
+namespace TeachMeMVCApp.Controllers
+{
+    public class UserAuthController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public UserAuthController(ApplicationDbContext context, 
+                                  UserManager<ApplicationUser> userManager, 
+                                  SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Login (LoginModel loginModel)
+        {
+            return View();
+        }
+
+    }
+}
